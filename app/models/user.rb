0a1045c29332251111e77b689e62d3a4d5687496
Ravi Singh <ravi@appsimpact.com>
2021-03-31 16:51:38 +0530

    working on styling bootstrap forms for devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
+
+  def display_name
+    email.split('@')[0].strip.capitalize
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
